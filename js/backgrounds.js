9d2a86d6a8129c3dde6c0dcab63c518cc3f50523
btam06 <btam06@gmail.com>
2014-07-08 00:18:08 -0700

    Changed images to icon fonts
    Thanks Dave.

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,10 +1,11 @@
 //Background parallax
 window.onscroll = function() {
 	var scrollHeight = window.pageYOffset;
+
+
 	var backgrounds = document.getElementsByClassName('panel');
-	
 	for(var i = 0; i < backgrounds.length; i++) {
-		backgrounds[i].style.backgroundPosition =  "center " + (scrollHeight - backgrounds[i].offsetTop) +"px";
+		backgrounds[i].style.backgroundPosition =  "center " + (scrollHeight - backgrounds[i].offsetTop + backgrounds[i].clientHeight/4) +"px";
 	}
 }
 
